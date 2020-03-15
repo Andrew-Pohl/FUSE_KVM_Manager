a298c1b0e78cfa4b424d20ff33f0fc16dceed917
Andrew Pohl <andypohl@hotmail.co.uk>
2020-03-15 11:30:40 +0000

    Add comments

--- FUSE_KVM_creator.sh
+++ FUSE_KVM_creator.sh
@@ -4,10 +4,11 @@ OLDIFS=$IFS
 IFS=','
 LASTVALIDATORINLIST=''
 
+#download script depends
 sudo apt-get install -y virt-manager
 sudo apt-get install -y sshpass
 
-
+#look through the list and take the last kvm name
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 while read validator ip ethaddr
 do
@@ -17,12 +18,14 @@ IFS=$OLDIFS
 
 echo "The last validator in the list $LASTVALIDATORINLIST"
 
+#strip the v off the front of the kvm name
 stripped="${LASTVALIDATORINLIST:1:${#LASTVALIDATORINLIST}-1}"
 
 new="v$((stripped + 1))"
 
 echo "new vlaidator KVM: $new"
 
+#install the new kvm
 virt-install \
 --name "$new" \
 --ram 2200 \
@@ -38,6 +41,7 @@ virt-install \
 --initrd-inject=ks-1804-minimalvm.cfg \
 --extra-args "ks=file:/ks-1804-minimalvm.cfg console=tty0 console=ttyS0,115200n8"
 
+#pull the ip address from the mac address of the new vm
 MAC=$(virsh domiflist $new | awk '{ print $5 }' | tail -2 | head -1)
 IP=$(arp -a | grep $MAC | awk '{ print $2 }' | sed 's/[()]//g')
 
@@ -48,6 +52,7 @@ echo ""$new","$IP",0" >> $INPUT
 USER='ubuntu'
 PASSWORD='ChangeMe'
 
+#ssh onto the new kvm and set it up
 sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$USER"@"$IP" << EOF
 echo "$PASSWORD" | sudo -S apt-get update;
 echo "$PASSWORD" | sudo -S apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common;
@@ -68,8 +73,10 @@ echo "$PASSWORD" | sudo -S chmod 777 /home/"$USER"/fuse-validator/quickstart.sh;
 ls;
 EOF
 
+#send over your .env file to the kvm
 sshpass -p "$PASSWORD" scp .env "$USER"@"$IP":/home/"$USER"/fuse-validator
 
+#start the quickstart
 sshpass -p "$PASSWORD" ssh -T "$USER"@"$IP" << EOF
 cd fuse-validator; 
 echo "$PASSWORD" | sudo -S /home/"$USER"/fuse-validator/quickstart.sh;
