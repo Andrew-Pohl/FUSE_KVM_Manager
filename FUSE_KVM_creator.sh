723b6769a47c36007d49a6dec888a8047f18288e
Andrew Pohl <andypohl@hotmail.co.uk>
2020-03-15 11:19:57 +0000

    Inital commit
    KVM script used to automatically create a ubuntu 18.04 KVM and set it up ready for validation

---
+++ FUSE_KVM_creator.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+INPUT=validator_list.csv
+OLDIFS=$IFS
+IFS=','
+LASTVALIDATORINLIST=''
+
+sudo apt-get install -y virt-manager
+sudo apt-get install -y sshpass
+
+
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read validator ip ethaddr
+do
+	LASTVALIDATORINLIST=$validator
+done < $INPUT
+IFS=$OLDIFS
+
+echo "The last validator in the list $LASTVALIDATORINLIST"
+
+stripped="${LASTVALIDATORINLIST:1:${#LASTVALIDATORINLIST}-1}"
+
+new="v$((stripped + 1))"
+
+echo "new vlaidator KVM: $new"
+
+virt-install \
+--name "$new" \
+--ram 2200 \
+--disk path=/var/lib/libvirt/images/"$new".img,size=25 \
+--vcpus 1 \
+--virt-type kvm \
+--os-type linux \
+--os-variant ubuntu18.04 \
+--graphics none \
+--location 'http://archive.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64/' \
+--noautoconsole \
+--wait=-1 \
+--initrd-inject=ks-1804-minimalvm.cfg \
+--extra-args "ks=file:/ks-1804-minimalvm.cfg console=tty0 console=ttyS0,115200n8"
+
+MAC=$(virsh domiflist $new | awk '{ print $5 }' | tail -2 | head -1)
+IP=$(arp -a | grep $MAC | awk '{ print $2 }' | sed 's/[()]//g')
+
+echo "IP OF $new IS $IP"
+
+echo ""$new","$IP",0" >> $INPUT
+
+USER='ubuntu'
+PASSWORD='ChangeMe'
+
+sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$USER"@"$IP" << EOF
+echo "$PASSWORD" | sudo -S apt-get update;
+echo "$PASSWORD" | sudo -S apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common;
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
+echo "$PASSWORD" | sudo -S apt-key add -;
+echo "$PASSWORD" | sudo -S apt-key fingerprint 0EBFCD88;
+echo "$PASSWORD" | sudo -S add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable";
+echo "$PASSWORD" | sudo -S apt-get update;
+echo "$PASSWORD" | sudo -S apt-get install -y docker-ce;
+echo "$PASSWORD" | sudo -S docker run hello-world;
+echo "$PASSWORD" | sudo -S curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
+echo "$PASSWORD" | sudo -S chmod +x /usr/local/bin/docker-compose;
+docker-compose --version;
+mkdir fuse-validator;
+cd fuse-validator;
+echo "$PASSWORD" | sudo -S wget -O /home/"$USER"/fuse-validator/quickstart.sh https://raw.githubusercontent.com/fuseio/fuse-network/master/scripts/quickstart.sh;
+echo "$PASSWORD" | sudo -S chmod 777 /home/"$USER"/fuse-validator/quickstart.sh;
+ls;
+EOF
+
+sshpass -p "$PASSWORD" scp .env "$USER"@"$IP":/home/"$USER"/fuse-validator
+
+sshpass -p "$PASSWORD" ssh -T "$USER"@"$IP" << EOF
+cd fuse-validator; 
+echo "$PASSWORD" | sudo -S /home/"$USER"/fuse-validator/quickstart.sh;
+echo "$PASSWORD" | sudo -S /home/"$USER"/fuse-validator/quickstart.sh;
+EOF
+
